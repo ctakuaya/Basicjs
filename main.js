e7273d2228c4d392a341c2a5e0745c60864359fa
taku collins <ctaku46@gmail.com>
2024-09-03 20:23:17 +0100

    Basic of java script

---
+++ main.js
@@ -0,0 +1,43 @@
+
+
+
+alert("Hellow JS");
+
+console.log("Hellow JS to java");
+
+let  age = 30 ;
+console.log(age);
+
+const fname = "Taku Collins";
+
+var htm = 25;
+console.log(htm);
+
+const cs = 234;
+const namex = "coll" ;
+
+const aya4 = null ;
+const aya5 = null ;
+
+const flo = 0.5;
+
+let coll = "yersterday";
+
+console.log(aya5);
+
+var aya5m = "todayhhhh";
+console.log(aya5);
+
+console.log(typeof cs);
+console.log(typeof aya4);
+console.log(typeof namex);
+console.log(typeof coll);
+console.log(typeof flo);
+
+
+console.log( "i am called " + fname + " with " + age + " today ");
+
+const  phrase = `my name is  ${fname} and i am ${age} years old`;
+console.log(phrase);
+console.log(fname.toUpperCase());
+console.log(fname.split());
